da23e28d219916851e425da84245961e7bb0e730
Tun Lin Naing <mctunlynn@gmail.com>
2022-12-03 22:30:15 +0630

    initial project

---
+++ ElectricBillWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ElectricBillWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
